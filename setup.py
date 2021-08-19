bd9a27ecd97310cf214444a4fab080abd0694410
Netanel Revah <netanel@areionsec.com>
2021-08-19 16:24:04 +0300

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 main_namespace = {}
-with open("uploader/version.py") as version_file:
+with open("filesff/version.py") as version_file:
     exec(version_file.read(), main_namespace)
 version = main_namespace["__version__"]
 
