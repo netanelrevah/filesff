ca895631420b1344b1cb555c09d3ee16f55843d5
Netanel Revah <netanelrevah@outlook.com>
2023-01-09 23:41:54 +0200

    readme

--- filesff/accessors/csvs.py
+++ filesff/accessors/csvs.py
@@ -3,9 +3,16 @@ from filesff.core.handlers import FileHandle
 from filesff.formatters.csvs import CsvDictFormatter
 
 
-def csv_dicts(file_path, file_handle_cls=FileHandle):
+def csv_dicts_file(file_path, file_handle_cls=FileHandle):
     return ContinuousFileAccessor.of(
         file_path=file_path,
+        formatter=CsvDictFormatter(),
+        file_handle_cls=file_handle_cls,
+    )
+
+
+def csv_dicts_temp_file(file_handle_cls=FileHandle):
+    return ContinuousFileAccessor.of_temp(
         formatter=CsvDictFormatter(),
         file_handle_cls=file_handle_cls,
     )
--- filesff/accessors/jsons.py
+++ filesff/accessors/jsons.py
@@ -11,9 +11,23 @@ def json_file(file_path, file_handle_cls=FileHandle, indentation=2):
     )
 
 
+def json_temp_file(file_handle_cls=FileHandle, indentation=2):
+    return FileAccessor.of_temp(
+        formatter=JsonFormatter(indentation=indentation),
+        file_handle_cls=file_handle_cls,
+    )
+
+
 def json_lines_file(file_path, file_handle_cls=FileHandle):
     return ContinuousFileAccessor.of(
         file_path=file_path,
+        formatter=JsonLinesFormatter(),
+        file_handle_cls=file_handle_cls,
+    )
+
+
+def json_lines_temp_file(file_handle_cls=FileHandle):
+    return ContinuousFileAccessor.of_temp(
         formatter=JsonLinesFormatter(),
         file_handle_cls=file_handle_cls,
     )
